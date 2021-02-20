0b8adfd3e60a1f288281a461470f23eb36b2de29
[REDACTED] <vikee.email@gmail.com>
2021-02-20 09:50:31 +0530

    Added utilities for anime search and pagination.

--- utils/anime_search.py
+++ utils/anime_search.py
@@ -1,20 +1,24 @@
-import html
 import asyncio
-import requests
+import datetime
+import html
+import random
+import re
+from contextlib import suppress
+
 import discord
 import discord.ext.commands
-
-from contextlib import suppress
+import requests
 from discord.ext.commands import Context
 
 from utils import embeds
+
 
 async def find_anime(search_term):
     URL = 'https://graphql.anilist.co'
     query = '''
     query($term: String) {
         Page(page:1, perPage:10) {
-            media(search:$term, type:ANIME, sort:POPULARITY_DESC) {
+            media(search:$term, type:ANIME, sort:TRENDING_DESC) {
             id
             idMal
             title {
@@ -47,53 +51,70 @@ async def find_anime(search_term):
     variables = {
         'term': search_term
     }
+
     response = requests.post(
         URL, json={'query': query, 'variables': variables})
-    return response.json()["data"]["Page"]
+    if response.status_code == 200:
+        return response.json()["data"]["Page"]
+    else:
+        return
+
 
 def get_page(search_result_list, page_no, ctx: Context) -> discord.Embed:
     if page_no >= len(search_result_list) or page_no < 0:
         return None
 
     anime_data = search_result_list[page_no]
-    
+
     embed = embeds.make_embed(title="Anime Search", context=ctx, color='gold')
 
     try:
-        embed = embeds.make_embed(title="Anime Search", context=ctx, color='gold')
+        embed = embeds.make_embed(
+            title="Anime Search", context=ctx, color='gold')
         embed.set_thumbnail(url=anime_data['coverImage']['extraLarge'])
-        
-        anime_name = anime_data['title']['english']+"("+anime_data['title']['romaji']+")"
+
+        try:
+            anime_name = anime_data['title']['english'] + \
+                "("+anime_data['title']['romaji']+")"
+
+        except:
+            anime_name = anime_data['title']['userPreferred']
+
         embed.add_field(name="Anime Name", value=anime_name, inline=False)
-        
-        description = html.unescape(anime_data['description'])
+
+        description = re.sub(r'<.*?>', '', anime_data['description'])
         if len(description) > 512:
             description = description[0:512] + "..."
-        
+
         embed.add_field(name="Description", value=description, inline=False)
 
-        embed.add_field(name="URL", value=anime_data['siteUrl'])
+        embed.add_field(name="URL", value=anime_data['siteUrl'], inline=False)
 
         if(anime_data['nextAiringEpisode']):
-            embed.add_field(name="Next Airing Episode", value=anime_data['nextAiringEpisode']['episode'], inline=True)
-            embed.add_field(name="Next Episode In", value=anime_data['nextAiringEpisode']['timeUntilAiring'])
+            embed.add_field(name="Next Airing Episode",
+                            value=anime_data['nextAiringEpisode']['episode'], inline=True)
+            embed.add_field(name="Next Episode In", value=str(datetime.timedelta(
+                seconds=int(anime_data['nextAiringEpisode']['timeUntilAiring']))))
 
     except:
         return None
 
     return embed
 
-async def anime_paginator(search_results ,ctx: Context):
+
+async def anime_paginator(search_results, ctx: Context):
     FIRST_EMOJI = "\u23EE"   # [:track_previous:]
     LEFT_EMOJI = "\u2B05"    # [:arrow_left:]
     RIGHT_EMOJI = "\u27A1"   # [:arrow_right:]
     LAST_EMOJI = "\u23ED"    # [:track_next:]
     DELETE_EMOJI = "⛔"  # [:trashcan:]
+    SAVE_EMOJI = "💾"  # [:floppy_disk:]
 
     bot = ctx.bot
     timeout = 30
 
-    PAGINATION_EMOJI = (FIRST_EMOJI, LEFT_EMOJI, RIGHT_EMOJI, LAST_EMOJI, DELETE_EMOJI)
+    PAGINATION_EMOJI = (FIRST_EMOJI, LEFT_EMOJI, RIGHT_EMOJI,
+                        LAST_EMOJI, DELETE_EMOJI, SAVE_EMOJI)
 
     page_no = 0
     msg = await ctx.send(embed=get_page(search_results, page_no, ctx))
@@ -104,25 +125,29 @@ async def anime_paginator(search_results ,ctx: Context):
     def check(reaction: discord.Reaction, user: discord.Member) -> bool:
         if reaction.emoji in PAGINATION_EMOJI and user == ctx.author:
             return True
-        
+
         return False
-    
+
     while True:
         try:
             reaction, user = await bot.wait_for("reaction_add", timeout=timeout, check=check)
-        
-        except asyncio.TimeoutError:
-            break
 
+        except asyncio.TimeoutError:
+            await msg.delete()
+            break
 
         if str(reaction.emoji) == DELETE_EMOJI:
             await msg.delete()
             break
 
+        if str(reaction.emoji) == SAVE_EMOJI:
+            await msg.clear_reactions()
+            break
+
         if reaction.emoji == FIRST_EMOJI:
             await msg.remove_reaction(reaction.emoji, user)
             page_no = 0
-        
+
         if reaction.emoji == LAST_EMOJI:
             await msg.remove_reaction(reaction.emoji, user)
             page_no = len(search_results) - 1
@@ -141,17 +166,15 @@ async def anime_paginator(search_results ,ctx: Context):
             if page_no >= len(search_results) - 1:
                 continue
 
-            page_no+=1
-        
+            page_no += 1
+
         embed = get_page(search_results, page_no, ctx)
-        
+
         if embed is not None:
             await msg.edit(embed=embed)
 
 
-
-
-
-    
-
-            
+def get_pat_gif():
+    url1 = "https://nekos.life/api/v2/img/pat"
+    url2 = "https://waifu.pics/api/sfw/pat"
+    return random.choice([requests.get(url=url1).json()['url'], requests.get(url=url2).json()['url']])
